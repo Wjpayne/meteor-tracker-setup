78f899c39e141e0cac53ef4c38ddeab61e6b9fa1
William Payne <payne.j.will@gmail.com>
2021-04-24 15:05:50 -0500

    add TODO for mongoDB

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
 
+//TODO setup mongodb cluster and add MONGO_URL .env
+
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
